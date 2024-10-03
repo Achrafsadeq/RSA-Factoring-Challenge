7d8ddd4e4dec3f1dab9d47251a3380af1c5abf51
Achrafsadeq <achrafsadeqq@gmail.com>
2024-10-03 10:15:37 -0700

    update

--- factors.c
+++ factors.c
@@ -3,67 +3,22 @@
 #include <math.h>
 
 /**
- * gcd - Calculate the greatest common divisor of two numbers
- * @a: First number
- * @b: Second number
- *
- * Return: The greatest common divisor of a and b
- */
-long long gcd(long long a, long long b)
-{
-	if (b == 0)
-		return (a);
-	return (gcd(b, a % b));
-}
-
-/**
- * pollards_rho - Implement the Pollard's rho algorithm for factorization
- * @n: Number to factorize
- *
- * Return: A factor of n
- */
-long long pollards_rho(long long n)
-{
-	if (n % 2 == 0)
-		return (2);
-
-	long long x = rand() % (n - 2) + 2;
-	long long y = x;
-	long long c = rand() % (n - 1) + 1;
-	long long d = 1;
-
-	while (d == 1)
-	{
-		x = (x * x + c) % n;
-		y = (y * y + c) % n;
-		y = (y * y + c) % n;
-		d = gcd(llabs(x - y), n);
-
-		if (d == n)
-			return (pollards_rho(n));
-	}
-
-	return (d);
-}
-
-/**
- * factorize - Factorize a number and print the result
- * @n: Number to factorize
+ * factorize - Find and print the smallest factor of a number
+ * @n: The number to factorize
  */
 void factorize(long long n)
 {
-	if (n == 1)
+	long long i;
+
+	for (i = 2; i <= sqrt(n); i++)
 	{
-		printf("1=1*1\n");
-		return;
+		if (n % i == 0)
+		{
+			printf("%lld=%lld*%lld\n", n, n / i, i);
+			return;
+		}
 	}
-
-	long long factor = pollards_rho(n);
-
-	if (factor == n)
-		printf("%lld is prime\n", n);
-	else
-		printf("%lld=%lld*%lld\n", n, factor, n / factor);
+	printf("%lld=%lld*1\n", n, n);
 }
 
 /**
@@ -83,14 +38,14 @@ int main(int argc, char *argv[])
 	if (argc != 2)
 	{
 		fprintf(stderr, "Usage: %s <file>\n", argv[0]);
-		return (1);
+		exit(1);
 	}
 
 	file = fopen(argv[1], "r");
 	if (file == NULL)
 	{
 		perror("Error opening file");
-		return (1);
+		exit(1);
 	}
 
 	while ((read = getline(&line, &len, file)) != -1)
